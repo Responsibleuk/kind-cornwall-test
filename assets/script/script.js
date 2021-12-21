7a6650b1e940d1185e49191cd251803200e8ddf8
Responsibleuk <james@we-are-responsible.com>
2021-12-21 17:36:14 +0000

    add

--- assets/script/script.js
+++ assets/script/script.js
@@ -88,15 +88,3 @@ const months = [
   //set initial values
   getRemaindingTime();
 
-// studio accordian
-
-
-const accordion_items_elms = document.querySelectorAll('.accordion .accordion__item');
-
-accordion_items_elms.forEach(accordionItem => {
-
-  accordionItem.querySelector('.accordion__item__head').addEventListener('click', (e) => {
-    accordionItem.classList.toggle('open');
-  });
-
-});
